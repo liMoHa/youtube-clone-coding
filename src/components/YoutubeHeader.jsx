afe107743a7f9533b19e080e4e60d52bf1ae3ac7
liMoHa <20195233@hallym.ac.kr>
2022-03-27 22:23:30 +0900

    feat: refresh when the logo clicked

--- src/components/YoutubeHeader.jsx
+++ src/components/YoutubeHeader.jsx
@@ -4,10 +4,10 @@ import styles from '../style/youtubeHeader.module.css';
 const YoutubeHeader = () => {
     return (
         <header className={styles.ytheader}>
-            <div className={styles.ytheader__logo}>
+            <a href='/' className={styles.ytheader__logo}>
                 <img src="./images/logo.png" alt="youtube logo" className={styles.ytheader__img} />
                 <span className={styles.ytheader__title}>YouTube</span>
-            </div>
+            </a>
             <form className={styles.ytheader__search}>
                 <input type="text" placeholder='Search..' className={styles.ytheader__input}/>
                 <button className={styles.ytheader__button}><img src="./images/search.png" alt="search icon" /></button>
